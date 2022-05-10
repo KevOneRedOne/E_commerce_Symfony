e6716d34a8eed370b1a23af5eb753aa90b3187a1
DamienDrozd <damien.drozd@icloud.com>
2022-05-10 12:45:45 +0200

    admin panel

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -148,4 +148,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->username;
+    }
 }
